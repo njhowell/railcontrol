66952f9ab27b8c7be8d4991a1a4b65d8098d4fa7
Nick Howell <nick@nickhowell.co.uk>
2015-07-25 12:18:03 +0100

    Add track image. Add a toggle method to switch between states which are now stored in the database. Overlay a toggle button and two indicators over the track image.

--- railcontrol.py
+++ railcontrol.py
@@ -5,6 +5,7 @@ import json
 urls = (
 	'/api/open/(\d+)', 'open',
 	'/api/close/(\d+)', 'close',
+	'/api/toggle/(\d+)', 'toggle',
 	'/(.*)', 'index'
 )
 
@@ -25,6 +26,7 @@ class open:
 		
 			if devpresent:
 				switch.operatepoint(motor.i2caddress,motor.i2cbank,motor.openiopin)
+			db.update('pointmotors', where="id=$motorid", currentstate='open', vars=locals())
 
 			pyDict = {'motorid':motor.id, 'openiopin':motor.openiopin, 'closeiopin':motor.closeiopin,'i2caddress':motor.i2caddress,'i2cbank':motor.i2cbank,'state':'open'}
 
@@ -43,8 +45,34 @@ class close:
 			
 			if devpresent:
 				switch.operatepoint(motor.i2caddress,motor.i2cbank,motor.closeiopin)
+			db.update('pointmotors', where="id=$motorid", currentstate='closed', vars=locals())
 
 			pyDict = {'motorid':motor.id, 'openiopin':motor.openiopin, 'closeiopin':motor.closeiopin,'i2caddress':motor.i2caddress,'i2cbank':motor.i2cbank,'state':'closed'}
+
+			web.header('Content-Type', 'application/json')
+			return json.dumps(pyDict)
+		except IndexError:
+			pyDict = {'error':'No such motor found'}
+
+			web.header('Content-Type', 'application/json')
+			return json.dumps(pyDict)
+
+class toggle:
+	def GET(self, motorid):
+		try:
+			motor = db.select('pointmotors', where="id=$motorid", vars=locals())[0]
+			if motor.currentstate == 'open':
+				iopin = motor.closeiopin
+				newstate = "closed"
+			else:
+				iopin = motor.openiopin
+				newstate = "open"
+
+			if devpresent:
+				switch.operatepoint(motor.i2caddress,motor.i2cbank,iopin)
+			db.update('pointmotors', where="id=$motorid", currentstate=newstate, vars=locals())
+
+			pyDict = {'motorid':motor.id, 'openiopin':motor.openiopin, 'closeiopin':motor.closeiopin,'i2caddress':motor.i2caddress,'i2cbank':motor.i2cbank,'state':newstate}
 
 			web.header('Content-Type', 'application/json')
 			return json.dumps(pyDict)
