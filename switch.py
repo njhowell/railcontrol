83313fbfc50001cc4103e054d8d0aac181be390c
Nick Howell <nick@nickhowell.co.uk>
2015-07-26 11:15:45 +0000

    change perms on database invert relay control so we can use the default open terminal on the relay. This stops the motors operating when first powered on.

--- switch.py
+++ switch.py
@@ -9,9 +9,12 @@ def setupio(busid,i2caddress):
 	address = i2caddress
 	__bus__.write_byte_data(address,0x00,0x00)
 	__bus__.write_byte_data(address,0x01,0x00)
+	__bus__.write_byte_data(address,0x12,255)
+	__bus__.write_byte_data(address,0x13,255)
 
 def operatepoint(i2caddress, bank, iopin):
 	global __bus__
+	iopin = 255 - iopin
 	__bus__.write_byte_data(i2caddress,bank,iopin)
 	time.sleep(0.1)
-	__bus__.write_byte_data(i2caddress,bank,0)
+	__bus__.write_byte_data(i2caddress,bank,255)
